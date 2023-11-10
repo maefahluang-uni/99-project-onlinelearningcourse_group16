656d6628e64e28f868441c030192c821246536d2
Trerat khampanya 30 <122779608+Trerat30@users.noreply.github.com>
2023-11-10 19:20:33 +0700

    fix

---
+++ src/main/java/th/mfu/Controller/UserController.java
@@ -0,0 +1,29 @@
+package th.mfu.Controller;
+
+import org.apache.catalina.User;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import th.mfu.Repository.UserRepository;
+
+@RestController
+public class UserController {
+    @GetMapping("/register")
+    public String getRegpeg(@ModelAttribute("user") User user) {
+        return "register";
+    }
+
+    @PostMapping(value = "/register")
+    public String saveUser(@ModelAttribute("user") User user, Model model) {
+        UserRepository.save(user);
+        model.addAttribute("message", "submit");
+        return "register";
+
+
+    }
+}
---
+++ src/main/java/th/mfu/Model/User.java
@@ -0,0 +1,58 @@
+package th.mfu.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String name;
+    private String password;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+
+    }
+
+}
