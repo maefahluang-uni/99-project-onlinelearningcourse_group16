155c9214d8b819faf30e724c010b69aa296de8f2
Trerat khampanya 30 <122779608+Trerat30@users.noreply.github.com>
2023-11-12 20:01:56 +0700

    fix

---
+++ src/main/java/th/mfu/Repository/TutorRepository.java
@@ -0,0 +1,8 @@
+package th.mfu.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import th.mfu.Model.Tutor;
+
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+
+}
