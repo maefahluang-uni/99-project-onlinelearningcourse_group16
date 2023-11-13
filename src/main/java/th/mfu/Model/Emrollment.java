9db4bf0d64f015afd9a0ed23253ab7fa0069be25
PimphaWongpracha059 <6531503059@lamduan.mfu.ac.th>
2023-11-13 12:20:20 +0700

    detail

--- src/main/java/th/mfu/Model/Emrollment.java
+++ src/main/java/th/mfu/Model/Emrollment.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 public class Emrollment {
     private Long enrollmentId;
 
-
+//slhak
     private LocalDate enrollmentDate;
     private User userName;
     private Course course;
