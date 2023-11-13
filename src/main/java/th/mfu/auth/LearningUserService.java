b36040b367b1c486eb0e8dd9870af51ecf67f026
Trerat khampanya 30 <122779608+Trerat30@users.noreply.github.com>
2023-11-13 15:02:13 +0700

    Merge branch 'main' of https://github.com/maefahluang-uni/99-project-onlinelearningcourse_group16

--- src/main/java/th/mfu/auth/LearningUserService.java
+++ src/main/java/th/mfu/auth/LearningUserService.java
@@ -1,26 +1,28 @@
 package th.mfu.auth;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-<<<<<<<< HEAD:src/main/java/th/mfu/auth/LearningUserDetailsService.java
+
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import th.mfu.AuthGroupRepository;
 
-public class LearningUserDetailsService implements UserDetailsService {
-========
+public class LearningUserService implements UserDetailsService {
+
 
 public class LearningUser implements UserDetailsService {
->>>>>>>> 5e90eabfcaada4b4169f94f73c5ef6cfe5b75ff4:src/main/java/th/mfu/auth/LearningUser.java
+
 
         private final UserRepository userRepository;
         private final AuthGroupRepository authGroupRepository;
 
-<<<<<<<< HEAD:src/main/java/th/mfu/auth/LearningUserDetailsService.java
-        public LearningUserDetailsService(UserRepository userRepository, AuthGroupRepository authGroupRepository) {
-========
+
+        public LearningUserService(UserRepository userRepository, AuthGroupRepository authGroupRepository) {
+
         public LearningUser(UserRepository userRepository, AuthGroupRepository authGroupRepository) {
->>>>>>>> 5e90eabfcaada4b4169f94f73c5ef6cfe5b75ff4:src/main/java/th/mfu/auth/LearningUser.java
+
             super();
             this.userRepository = userRepository;
             this.authGroupRepository = authGroupRepository;
