eea77d701b7e95f13281540b3f44ca3d08b536f7
Trerat khampanya 30 <122779608+Trerat30@users.noreply.github.com>
2023-11-15 16:41:23 +0700

    skjdj

--- src/main/java/th/mfu/auth/AuthGroupRepository.java
+++ src/main/java/th/mfu/auth/AuthGroupRepository.java
@@ -1,7 +1,11 @@
 package th.mfu.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 import java.util.List;
+
 
 public interface AuthGroupRepository extends JpaRepository<AuthGroup, Long> {
     List<AuthGroup> findByUsername(String username);
