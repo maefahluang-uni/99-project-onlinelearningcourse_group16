103f64b707ee2d88ddcbe44f06d563f5035b984c
Trerat khampanya 30 <122779608+Trerat30@users.noreply.github.com>
2023-11-22 13:09:18 +0700

    fix

--- src/main/java/th/mfu/App.java
+++ src/main/java/th/mfu/App.java
@@ -2,7 +2,12 @@ package th.mfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"th.mfu.auth" , "th.mfu.Controller" , "th.mfu.dto" , "th.mfu.Model" , "th.mfu.security", "service"})
+@EnableJpaRepositories(basePackages = "th.mfu.Repository")
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
